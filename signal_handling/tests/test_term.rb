0ef5833481d84940b37aef9633288edcaab94894
woodprogrammer <asddeneme3@gmail.com>
2018-01-03 03:03:46 +0200

    Test Dockerfiles Changed

--- signal_handling/tests/test_term.rb
+++ signal_handling/tests/test_term.rb
@@ -2,6 +2,11 @@ trap('INT') do #if user have keyboard interrupt
     puts 'Exiting...'
     exit
 end
+i = 0
+loop do
+	i += 1
+end
+
 trap('TERM') do #if instance is down
     puts 'Term Incoming ..'
     exit
